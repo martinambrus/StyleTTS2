d9ef6f54d8fcff16404e8e169fe29aaa53bc1885
Martin Ambrus <martinambrus@users.noreply.github.com>
2025-05-06 11:15:28 -0700

    emoji removal didn't prove fruitful

--- text_utils.py
+++ text_utils.py
@@ -13,22 +13,11 @@ dicts = {}
 for i in range(len((symbols))):
     dicts[symbols[i]] = i
 
-def remove_emojis(msg):
-    emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags=re.UNICODE)
-    msg = emoji_pattern.sub(r'', msg)
-    return msg
-
 class TextCleaner:
     def __init__(self, dummy=None):
         self.word_index_dictionary = dicts
         print(len(dicts))
     def __call__(self, text):
-        text = remove_emojis(text)
         indexes = []
         for char in text:
             try:
